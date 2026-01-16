c64ec324387771a515184ef8b016f15e8584bfdf
Håvard <36506751+risingbridge@users.noreply.github.com>
2026-01-16 18:45:08 +0100

    Update const.py

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
 DOMAIN = "cavok"
 VERSION = "0.1.0"
-USER_AGENT = f"RisebroWeatherApp haavard@risebro.net"
+USER_AGENT = f"RisebroWeatherApp **************"
 # AcmeWeatherApp/0.9 github.com/acmeweatherapp
