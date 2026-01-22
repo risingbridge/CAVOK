39178eb00e1a878289d26a2971ef08b6b0a03641
Håvard <36506751+risingbridge@users.noreply.github.com>
2026-01-22 14:57:21 +0100

    Added CavokCalculatedReadFeel
    Added a sensor named CavokCalculatedReadFeel, that uses temperature and windspeed from the METAR to calculate the windchill effect. Not a METAR-thing, but I needed it for my setup.

--- sensor.py
+++ sensor.py
@@ -23,10 +23,12 @@ async def async_setup_entry(
         [
             CavokMetarSensor(coordinator, icao),
             CavokWindSpeedSensor(coordinator, icao),
+            CavokWindGustSensor(coordinator, icao),
             CavokWindDirectionSensor(coordinator, icao),
             CavokTemperatureSensor(coordinator, icao),
             CavokDewpointSensor(coordinator, icao),
             CavokMetarTimestamp(coordinator, icao),
+            CavokCalculatedReadFeel(coordinator, icao),
         ]
     )
 
@@ -110,6 +112,25 @@ class CavokWindSpeedSensor(CavokEntity):
         return None
 
 
+class CavokWindGustSensor(CavokEntity):
+    def __init__(self, coordinator, icao):
+        super().__init__(coordinator, icao)
+        self._attr_unique_id = f"{icao}_wind_gust"
+        self._attr_name = f"{icao} Wind Gust"
+        self._attr_icon = "mdi:weather-windy"
+        self._attr_native_unit_of_measurement = UnitOfSpeed.KNOTS
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+
+    @property
+    def native_value(self):
+        metar_object = self.coordinator.data.get("metar_object")
+        if metar_object is None or metar_object.data is None:
+            return None
+        if metar_object.data.wind_gust:
+            return metar_object.data.wind_gust.value
+        return None
+
+
 class CavokWindDirectionSensor(CavokEntity):
     def __init__(self, coordinator, icao):
         super().__init__(coordinator, icao)
@@ -165,4 +186,35 @@ class CavokDewpointSensor(CavokEntity):
             return None
         if metar_object.data.dewpoint:
             return metar_object.data.dewpoint.value
+        return None
+
+
+class CavokCalculatedReadFeel(CavokEntity):
+    def __init__(self, coordinator, icao):
+        super().__init__(coordinator, icao)
+        self._attr_unique_id = f"{icao}_real_feel"
+        self._attr_name = f"{icao} Real Feel (Calculated)"
+        self._attr_device_class = SensorDeviceClass.TEMPERATURE
+        self._attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
+        self._attr_state_class = SensorStateClass.MEASUREMENT
+
+    @property
+    def native_value(self):
+        metar_object = self.coordinator.data.get("metar_object")
+        if metar_object is None or metar_object.data is None:
+            return None
+        if metar_object.data.temperature and metar_object.data.wind_speed:
+            windSpeedKts = metar_object.data.wind_speed.value
+            windSpeedKmH = windSpeedKts * 1.852
+            temperature = metar_object.data.temperature.value
+            # Twc​=13.12+0.6215⋅T−11.37⋅V0.16+0.3965⋅T⋅V0.16
+            realfeel = (
+                13.12
+                + 0.6215 * temperature
+                - 11.37 * (windSpeedKmH**0.16)
+                + 0.3965 * temperature * (windSpeedKmH**0.16)
+            )
+            if temperature > 10 or windSpeedKmH < 4.8:
+                return temperature
+            return round(realfeel, 2) if realfeel is not None else None
         return None
